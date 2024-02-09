c7ea0335f2baf3c1fa5b9798171db6f6cb089479
nikanach <158843691+nikanach@users.noreply.github.com>
2024-02-09 18:00:14 +0400

    Add files via upload

---
+++ python course/day 1/lesson1.py
@@ -0,0 +1,33 @@
+name = "nika"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "nika" არის ცვლადისთვის მნიშვნელობა
+
+surname = "nachkpeia"
+
+# print(name)
+#პრინტ ფუნქციას გადაცემა ერკანზე გამოსატანი ობიექტი
+
+name = "nika" #ეს არის str (string) ტიპის ცვლადი
+age = 14 # ეს არის int (integer) მთელი რიცხვი
+height = 185 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True  #True an False
+is_ugly = False 
+
+isUgly = False 
+
+
+print(name +" "+ surname)
+
+# print(name + age)
+
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age))
